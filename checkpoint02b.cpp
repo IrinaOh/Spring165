c2b0a76d324c73eee6f797c792c1e73517bf13bf
ew1773 <ew1773@gmail.com>
2016-04-21 19:35:42 -0500

    Done, Not checked

--- checkpoint02b.cpp
+++ checkpoint02b.cpp
@@ -13,16 +13,36 @@ using namespace std;
 
 // TODO: Define your Complex struct here
 struct Complex {
-   double x;
-   double y;
+   double real;
+   double imaginary;
 };
 
 // TODO: Add your prompt function here
-
+/**********************************************************************
+ * Function: prompt
+ * Purpose: Prompts user for complex numbers.
+ ***********************************************************************/
+void prompt(Complex & x)
+{
+    cout << "Real: ";
+    cin >> x.real;
+    cout << "Imaginary: ";
+    cin >> x.imaginary; 
+  
+    cout << endl;
+    
+    return;
+}
 
 // TODO: Add your display function here
-
-
+/**********************************************************************
+ * Function: display
+ * Purpose: Displays the added complex numbers.
+ ***********************************************************************/
+void display(Complex x)
+{
+    cout << x.real << "+" << x.imaginary << "i" << endl;
+}
 /**********************************************************************
  * Function: addComplex
  * Purpose: Adds two complex numbers together and returns the sum.
@@ -30,8 +50,10 @@ struct Complex {
 Complex addComplex(const Complex & x, const Complex & y)
 {
    // TODO: Fill in the body of the add function
-
-
+   sum.real = c1.real + c2.real;
+   sum.imaginary = c1.imaginary + c2.imaginary;
+   
+   return sum;
 }
 
 
@@ -42,17 +64,18 @@ Complex addComplex(const Complex & x, const Complex & y)
 int main()
 {
    // Declare two Complex objects, c1 and c2
-   Complex x.c1;
+   Complex c1;
    Complex c2;
 
    // Call your prompt function twice to fill in c1, and c2 
-
+   prompt(Complex & c1);
+   prompt(Complex & c2);
 
    // Declare another Complex for the sum
-
+   Complex sum;
 
    // Call the addComplex function, putting the result in sum;
-
+   sum = addComplex(Complex & c1, Complex & c2);
    
    cout << "\nThe sum is: ";
    display(sum);
