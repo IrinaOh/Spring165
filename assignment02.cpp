90c387343e3befe78424879cc08edb9f6f268e39
ew1773 <ew1773@gmail.com>
2016-04-25 18:46:28 -0500

    Ready to turn in.

--- assignment02.cpp
+++ assignment02.cpp
@@ -4,16 +4,16 @@
 *    Brother McCracken, CS165
 * Author:
 *    Edgar Wright
-* Summary: 
-*    
+* Summary:  This program returns the files accessed between 
+*    the times specified. 
 *    
 *    
 *    
 *    
 *
-*    Estimated:  0.0 hrs   
-*    Actual:     0.0 hrs
-*      Please describe briefly what was the most difficult part.
+*    Estimated:  4.0 hrs   
+*    Actual:     5.0 hrs
+*      Setting up the struct array and I kept reading off the end.
 ************************************************************************/
 
 #include <iostream>
@@ -21,25 +21,28 @@
 #include <fstream>
 using namespace std;
 
-struct AccessRecord {
+struct AccessRecord 
+{
    long int timeStamp;
    char fileName[256];
    char user[256];
 };
 
-struct UserQuery {
+struct UserQuery 
+{
    long int startTime;
    long int endTime;
    char accessFile[256]; 
    int fileLength; 
-   int results[500];    
+   int results[500];
+   int numOfResults;    
 };
 
- void filePrompt(UserQuery & x);
- void timePrompt(UserQuery & x);
- void readFile(UserQuery & x, AccessRecord record[500]);
- void fileSearch(UserQuery & x, AccessRecord record[500]);
- void displayResults(UserQuery  x, AccessRecord record[500]);
+void filePrompt(UserQuery &x);
+void timePrompt(UserQuery &x);
+void readFile(UserQuery &x, AccessRecord record[500]);
+void fileSearch(UserQuery &x, AccessRecord record[500]);
+void displayResults(UserQuery  x, AccessRecord record[500]);
  
 /**********************************************************************
  * Function: main
@@ -47,8 +50,8 @@ struct UserQuery {
  ***********************************************************************/
 int main()
 {
- UserQuery uQ1; //uQ1 is user query one. 
- AccessRecord record[500];
+   UserQuery uQ1; //uQ1 is user query number one. 
+   AccessRecord record[500];
    
    filePrompt(uQ1);
    timePrompt(uQ1);
@@ -61,83 +64,85 @@ int main()
 
 /**********************************************************************
  * Function: filePrompt
- * Purpose: Prompts the user for a filename
+ * Purpose: Prompts the user for a file name
  ***********************************************************************/
- void filePrompt(UserQuery & x)
- {
-    cout << "Enter the access record file: ";
-    cin >> x.accessFile;
-    cout << endl;
-    return;
- }
+void filePrompt(UserQuery &x)
+{
+   cout << "Enter the access record file: ";
+   cin >> x.accessFile;
+   cout << endl;
+   return;
+}
  
- /**********************************************************************
+/**********************************************************************
  * Function: timePrompt
- * Purpose: Prompts the user for a start and end timestamp.
+ * Purpose: Prompts the user for a start and end time stamp.
  ***********************************************************************/
- void timePrompt(UserQuery & x)
- {
-    cout << "Enter the start time: ";
-    cin >> x.startTime;
-    cout << "Enter the end time: ";
-    cin >> x.endTime;
+void timePrompt(UserQuery &x)
+{
+   cout << "Enter the start time: ";
+   cin >> x.startTime;
+   cout << "Enter the end time: ";
+   cin >> x.endTime;
     
-    return;
- }
+   return;
+}
  
- /**********************************************************************
+/**********************************************************************
  * Function: readFile
  * Purpose: Reads file provides by user
  ***********************************************************************/
- void readFile(UserQuery & x, AccessRecord record[500])
- {
-    int i = 0;
-    ifstream fin(x.accessFile);  
-    if (fin.fail()) // check to see if the file correctly opened 
-      {
-         cout << "Unable to open: " << x.accessFile << endl;
-         return; 
-      }
+void readFile(UserQuery &x, AccessRecord record[500])
+{
+   int i = 0;
+   ifstream fin(x.accessFile);  
+   if (fin.fail()) // check to see if the file correctly opened 
+   {
+      cout << "Unable to open: " << x.accessFile << endl;
+      return; 
+   }
    char text[256];
-    while (!fin.eof())
-    {   
+   while (!fin.eof())
+   {   
       fin >> record[i].fileName;
       fin >> record[i].user;
       fin >> record[i].timeStamp;
       i++;
       x.fileLength = i; 
-    }
+   }
                  
     // close the file 
-    fin.close(); 
+   fin.close(); 
    
-  return;
- }
+   return;
+}
  
- /**********************************************************************
+/**********************************************************************
  * Function: fileSearch
  * Purpose: Searches and returns files that were accessed between 
  * start and end time stamps. 
  ***********************************************************************/
- void fileSearch(UserQuery & x, AccessRecord record[500])
- {
-         int j = 0;
-    for(int i = 0; i < x.fileLength; i++)
-    {
-       if ( x.startTime <= record[i].timeStamp && x.endTime >= record[i].timeStamp)
-       {
+void fileSearch(UserQuery &x, AccessRecord record[500])
+{
+   int j = 0;
+   for (int i = 0; i < x.fileLength; i++)
+   {
+      if ( x.startTime <= record[i].timeStamp 
+         && x.endTime >= record[i].timeStamp)
+      {
          x.results[j] = i;
          j++;
-       }
-    }
-    return;
- }
+      }
+   }
+   x.numOfResults = j;
+   return;
+}
  
- /**********************************************************************
+/**********************************************************************
  * Function: displayResults
  * Purpose: Displays results from search.
  ***********************************************************************/
- void displayResults(UserQuery  x, AccessRecord record[500])
+void displayResults(UserQuery  x, AccessRecord record[500])
 {
    cout << endl;
    cout << "The following records match your criteria:" << endl;
@@ -148,7 +153,7 @@ int main()
         << endl; 
    cout << "--------------- ------------------- -------------------" << endl;
 
-   for(int i = 0; i < x.fileLength; i++)
+   for (int i = 0; i < x.numOfResults; i++)
    {
       int tempI = x.results[i];
       cout << setw(15) << record[tempI].timeStamp 
