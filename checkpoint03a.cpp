a4dc619cd2156a2ef40e0f34ea36fd8781f8e5d1
ew1773 <ew1773@gmail.com>
2016-04-27 15:49:47 -0500

    check 3

---
+++ checkpoint03a.cpp
@@ -0,0 +1,22 @@
+/***********************************************************************
+* Program:
+*    Checkpoint 03a, Exceptions  
+*    Brother McCracken, CS165
+* Author:
+*    Edgar Wright
+* Summary: 
+*    Summaries are not necessary for checkpoint assignments.
+* ***********************************************************************/
+
+#include <iostream>
+using namespace std;
+
+/**********************************************************************
+ * Function: main
+ * Purpose: This is the entry point and driver for the program.
+ ***********************************************************************/
+int main()
+{
+  
+   return 0;
+}
